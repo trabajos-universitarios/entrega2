7bd56657ae3565271bcd540ae97ff517226ff2f1
CarlitoxOMG <102319247+CarlitoxOMG@users.noreply.github.com>
2023-05-22 22:54:27 -0400

    Eliminacion de archivos inecesarios segun adrian

--- proyecto_entrega/AppWebBA/apirest/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-from .views import productos_bodega_leer_todos
-
-urlpatterns = [
-    path('productos_bodega_leer_todos/', productos_bodega_leer_todos, name="productos_bodega_leer_todos"),
-]
